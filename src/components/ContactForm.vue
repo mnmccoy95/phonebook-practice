736489d71ac4c4b04246fc416953d0a44d3422eb
mnmccoy95 <mnmccoy95@gmail.com>
2021-03-08 11:52:23 -0600

    completed phonebook example

---
+++ src/components/ContactForm.vue
@@ -0,0 +1,68 @@
+<template>
+  <div>
+    <h3 class="teal--text">New Contact</h3>
+    <v-form @submit.prevent="handleSubmit" ref="contactForm">
+      <v-text-field
+        outlined
+        label="First Name"
+        v-model="form.firstName"
+        :rules="validators.firstName"
+      />
+      <v-text-field outlined label="Last Name" v-model="form.lastName" />
+      <v-text-field type="number" outlined label="Phone" v-model="form.phone" />
+      <v-select
+        outlined
+        label="Phone Type"
+        :items="phoneTypeOptions"
+        v-model="form.type"
+      />
+      <v-text-field outlined label="Email" v-model="form.email" />
+      <v-btn type="submit" color="teal" dark>Submit</v-btn>
+    </v-form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      form: {
+        firstName: "",
+        lastName: "",
+        phone: "",
+        type: "",
+        email: "",
+      },
+      phoneTypeOptions: ["Home", "Cell", "Office"],
+      validators: {
+        firstName: [
+          (val) => !!val || "Contact first name is required",
+          (val) =>
+            val.length < 25 || "First name must be less than 25 characters",
+        ],
+      },
+    };
+  },
+  methods: {
+    handleSubmit() {
+      const isValid = this.$refs.contactForm.validate();
+      if (!isValid) {
+        // Form is not valid. Exit the method
+        return;
+      }
+
+      this.$emit("contact-submit", this.form);
+      this.form = {
+        firstName: "",
+        lastName: "",
+        phone: "",
+        type: "",
+        email: "",
+      };
+      this.$refs.contactForm.resetValidation();
+    },
+  },
+};
+</script>
+
+<style></style>
---
+++ src/components/Phonebook.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <v-row>
+      <v-col :lg="8" :sm="12">
+        <contacts-table :contacts="contacts" />
+      </v-col>
+      <v-col :lg="4" :sm="12">
+        <contact-form @contact-submit="addContact" />
+      </v-col>
+    </v-row>
+  </div>
+</template>
+
+<script>
+import ContactForm from "./ContactForm.vue";
+import ContactsTable from "./ContactsTable.vue";
+
+export default {
+  components: { ContactForm, ContactsTable },
+  data() {
+    return {
+      contacts: [],
+    };
+  },
+  mounted() {
+    const existingContacts = JSON.parse(localStorage.getItem("contacts"));
+    this.contacts = existingContacts || [];
+  },
+  methods: {
+    addContact(newContact) {
+      this.contacts.push(newContact);
+      localStorage.setItem("contacts", JSON.stringify(this.contacts));
+    },
+  },
+};
+</script>
+
+<style></style>
